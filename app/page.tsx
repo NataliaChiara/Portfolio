41b947e7f1928d596ca66ec91f897f6fd9557d9d
Natalia Chiara <natalia@contenidos-digitales.com>
2024-09-07 23:02:22 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import s from "./page.module.css";
 import cs from 'classnames'
 import { useState } from "react";
+import { ReactSVG } from "react-svg";
 
 export default function Home() {
 
@@ -12,35 +13,53 @@ export default function Home() {
     '/images/js.png',
     '/images/ts.png',
     '/images/react.png',
-    '/images/next.png',
-    '/images/python.png',
-    '/images/django.png',
-    '/images/node.png',
+    '/images/next.png'
   ]
 
-  const certificados = [
+  const certifications = [
     '/images/certificado-js.jpeg',
-    '/images/certificado-react.jpeg',
-    '/images/certificado-python.jpeg'
+    '/images/certificado-python.jpeg',
+    '/images/certificado-react.jpeg'
   ]
 
   const works = [
     {
       video: '/videos/cinemark-hoyts-clon.mp4',
-      titulo: 'Cinemark-Hoyts Clon',
-      enlace: 'https://cinemark-hoyts-clon.vercel.app/',
-      descripcion: 'Clon de la web de Cinemark-Hoyts',
-      tecnologias: [
+      title: 'Cinemark-Hoyts Clon',
+      url: 'https://cinemark-hoyts-clon.vercel.app/',
+      description: 'Sitio web de práctica.',
+      logos: [
         '/images/ts.png',
-        '/images/react.png'
+        '/images/next.png'
       ]
     },
     {
       video: '/videos/facu-trainer-fit.mp4',
-      titulo: 'Facu Trainer Fit',
-      enlace: 'https://facutrainerfit.com.ar/',
-      descripcion: 'Landing page hecha en conjunto con un equipo de diseñadores y desarrolladores como proyecto final para el curso de React de Coderhouse.',
-      tecnologias: [
+      title: 'Facu Trainer Fit',
+      url: 'https://facutrainerfit.com.ar/',
+      description: 'Proyecto freelance hecho en conjunto con un equipo de diseñadores y desarrolladores.',
+      logos: [
+        '/images/ts.png',
+        '/images/next.png'
+      ]
+    },
+    // {
+    //   video: '/videos/nancy-alicia-rizzo-frontend.mp4',
+    //   title: 'Nancy Alicia Rizzo',
+    //   url: 'https://github.com/NataliaChiara/Nancy-Alicia-Rizzo-Frontend',
+    //   description: 'Landing page de práctica para psicologa hecha con Node para el backend, HTML, CSS y JS para el frontend y SQL para la database.',
+    //   logos: [
+    //     '/images/js.png',
+    //     '/images/node.png',
+    //     '/images/sql.png'
+    //   ]
+    // },
+    {
+      video: '/videos/luciana-castano.mp4',
+      title: 'Luciana Castaño',
+      url: 'http://www.luchicastano.com/',
+      description: 'Trabajo freelance autonomo de una landing page para una coaching, desde el diseño hasta el manejo del hosting.',
+      logos: [
         '/images/ts.png',
         '/images/next.png'
       ]
@@ -49,7 +68,10 @@ export default function Home() {
 
   return (
     <div className={isLight ? s.light_page : s.page}>
-      <button className={s.change_mode_btn} onClick={() => setIsLight(!isLight)}>{isLight? 'dark':'light'}</button>
+      <header className={s.header}>
+        <button onClick={() => setIsLight(!isLight)}>{isLight? 'dark':'light'}</button>
+        <button>change language</button>
+      </header>
       <main className={s.main}>
         
         <div className={s.main__section}>
@@ -57,31 +79,36 @@ export default function Home() {
             <div>
               <h1 className={s.main__section__content__title}>NATALIA CHIARA</h1>
               <h2 className={s.main__section__content__subtitle}>Frontend Developer</h2>
-              <div className={s.main__section__content__logos}>
+              <div className={s.logos}>
                 {logos.map((url)=>(
-                  <Image  className={s.main__section__content__logos__img} src={url} key={url} alt="technology-icon" width={30} height={30}/>
+                  <Image  className={s.logos__img} src={url} key={url} alt="technology-icon" width={30} height={30}/>
                 ))}
               </div>
             </div>
             <Image src='/images/perfil.png' alt="profile-picture" width={300} height={300}/>
+            <button className={s.main__section__content__down_btn}>flecha abajo</button>
           </div>
         </div>
-
+        
         {works.map((data, index) => {
-          const { video, titulo, enlace, descripcion, tecnologias } = data
+          const { video, title, url, description, logos } = data
           return (
             <div key={index} className={s.main__section}>
               <div className={cs(s.main__section__container, {[s.main__section__container__is_reverse] : index % 2 === 0})}>
-                <video autoPlay loop muted className={s.main__section__container__video}>
-                  <source src={video} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                <div className={s.main__section__container__video_container}>
+                  <ReactSVG className={s.main__section__container__video_container__svg} src="/icons/notebook_outline.svg"/>
+                  <video autoPlay loop muted className={s.main__section__container__video}>
+                    <source src={video} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+
                 <div className={s.main__section__container__info}>
-                  <h1><a href={enlace}><Image src='/images/link.png' width={20} height={20} alt='link-icon'/>{titulo}</a></h1>
-                  <p>{descripcion}</p>
-                  <div className={s.main__section__container__info__logos}>
-                    {tecnologias.map((url) => (
-                      <Image className={s.main__section__container__info__logos__img} src={url} key={url} alt="technology-icon" width={30} height={30}/>
+                  <h1><a href={url} target="__blank"><Image src='/images/link.png' width={20} height={20} alt='link-icon'/>{title}</a></h1>
+                  <p>{description}</p>
+                  <div className={s.logos}>
+                    {logos.map((url) => (
+                      <Image className={s.logos__img} src={url} key={url} alt="technology-icon" width={30} height={30}/>
                     ))}
                   </div>
                 </div>
@@ -90,9 +117,9 @@ export default function Home() {
           )
         })}
 
-        <div className={s.main__section}>
+        {/* <div className={s.main__section}>
           experiencia profesional / marcas
-        </div>
+        </div> */}
         
         <div className={s.main__section}>
           <div className={s.main__section__content}>
@@ -100,8 +127,8 @@ export default function Home() {
             <div className={s.main__section__content__certifications}>
             <h3>Certificaciones</h3>
               <div className={s.main__section__content__certifications__images}>
-                {certificados.map((url) => (
-                  <Image className={s.main__section__content__certifications__img} key={url} src={url} alt="certification" width={300} height={250}/>
+                {certifications.map((url) => (
+                  <Image className={s.main__section__content__certifications__images__img} key={url} src={url} alt="certification" width={300} height={250}/>
                 ))}
               </div>
             </div>
@@ -109,7 +136,7 @@ export default function Home() {
         </div>
       </main>
       <footer className={s.footer}>
-        contacto
+        contacto / descarga pdf - cv
       </footer>
     </div>
   );
